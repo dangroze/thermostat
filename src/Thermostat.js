1e837f6221c401de7dbd61d117cd8b4a17082860
Dan Groze <dangroze@gmail.com>
2019-02-07 16:47:16 +0000

    Passed test- minimum temp

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,6 +2,7 @@
 
 function Thermostat(){
   this._temperature = 20;
+  this.MIN_TEMPERATURE = 10;
 };
 Thermostat.prototype = {
   currentTemperature: function() {
@@ -11,6 +12,12 @@ Thermostat.prototype = {
     this._temperature += 1;
   },
   down: function() {
+    if (this.isMininumTemperature()) {
+      return;
+    };
     this._temperature -= 1;
+  },
+  isMininumTemperature: function() {
+    return this._temperature === this.MIN_TEMPERATURE;
   }
 };
