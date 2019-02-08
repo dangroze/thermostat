9d9e25ec1ad62385f4a1f4a9e1b0731b989dbd9c
Dan Groze <dangroze@gmail.com>
2019-02-08 09:40:43 +0000

    Passed test, implemented when powerSavingOn, max temp = 25, else 32

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,19 +3,31 @@
 function Thermostat(){
   this._temperature = 20;
   this.MIN_TEMPERATURE = 10;
+  this._powerSavingOn = true;
 };
 Thermostat.prototype = {
   currentTemperature: function() {
     return this._temperature;
   },
   up: function(temp) {
-    this._temperature += temp;
+    if(this._temperature + temp >= this.maxTemperature()){
+      this._temperature = this.maxTemperature();
+    }else {
+      this._temperature += temp;
+    }
   },
   down: function(temp) {
-    if(this._temperature - temp < this.MIN_TEMPERATURE) {
+    if (this._temperature - temp < this.MIN_TEMPERATURE) {
       this._temperature = this.MIN_TEMPERATURE;
     } else {
-    this._temperature -= temp;
+      this._temperature -= temp;
+    }
+  },
+  maxTemperature: function() {
+    if (this._powerSavingOn) {
+      return 25;
+    } else {
+      return 32;
     }
   }
 }
